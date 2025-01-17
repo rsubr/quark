7e4e31986dbfa6aa1ec36b1b59a43675b31e2201
Raja Subramanian <rajasuperman@gmail.com>
2025-01-17 13:22:04 +0530

    updated pip-compile

---
+++ pip-compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start an ephemeral docker container, and install pip-tools. Then run
+# pip-compile to generate the full requirements.txt so dependabot can identify
+# all the dependencies.
+
+BASEDIR='/app'
+
+if [ ! -f /.dockerenv ]; then
+  if ! command -v docker &> /dev/null; then
+    echo "Unable to find docker, exiting."
+    exit 1
+  fi
+  docker run -ti --rm -v ${PWD}:${BASEDIR} python:3.12-bookworm ${BASEDIR}/pip-compile.sh
+  exit 0
+fi
+
+set -eoux pipefail
+
+
+pip install pip-tools
+pip-compile --generate-hashes -v \
+    -o ${BASEDIR}/requirements.txt ${BASEDIR}/requirements.in
---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# git commands shortcut to tag the current commit
+
+ver="$1"
+
+if [ -z "$ver" ]; then
+  echo "Please provide a tag to replace"
+  exit 1
+fi
+
+set -eoux pipefail
+
+echo "Replace local tag ${ver} to reference latest commit"
+git tag -fa "${ver}" -m "Release ${ver}"
+
+echo "Deleting remote tag ${ver}"
+git push --delete origin "${ver}"
+
+echo "Pushing local tags to remote"
+git push origin tag "${ver}"
