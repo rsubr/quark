0ccb21162a99104bdb5a910c746912df08f7973c
Raja Subramanian <rajasuperman@gmail.com>
2025-03-15 00:29:59 +0530

    added portable native build

---
+++ build-portable.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Start an ephemeral docker container, and use miniconda to install a portable
+# version of python and quark dependencies that can run from /opt/quark.
+
+BASEDIR=/opt/quark
+
+if [ ! -f /.dockerenv ]; then
+  if ! command -v docker &> /dev/null; then
+    echo "Unable to find docker, exiting."
+    exit 1
+  fi
+
+  # Runs on docker host
+  set -eoux pipefail
+
+  # may need to sudo this to clean up the build directory
+  rm -rf ./build
+
+  docker run -ti --rm \
+    -v ${PWD}/build/opt:/opt -v ${PWD}:/app \
+    python:3.12-bookworm /app/build-portable.sh
+
+  # Create a tarball with portable quark
+  tar czvf ./build/quark.tar.gz -C ./build opt/quark
+
+  exit 0
+fi
+
+# Runs inside docker
+set -eoux pipefail
+
+wget -q -O /tmp/Miniconda3-latest-Linux-x86_64.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+# Install miniconda in /opt/minicoda
+bash /tmp/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda
+
+# Add conda environment details to bash
+/opt/miniconda/bin/conda init bash && . ~/.bashrc
+
+conda create --prefix $BASEDIR python=3.12 --yes
+conda activate $BASEDIR
+
+pip install -r /app/requirements.txt
+
+# Install playwright browsers and deps
+export PLAYWRIGHT_BROWSERS_PATH=$BASEDIR/pw-browsers
+playwright install --only-shell --with-deps chromium
